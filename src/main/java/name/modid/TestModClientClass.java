bad23955757ab919aceae5c7698a4be35967f261
Andrew Wisniewski <andrew.wisniewski.20@cnu.edu>
2026-02-01 17:36:43 -0500

    Initial commit

---
+++ src/main/java/name/modid/TestModClientClass.java
@@ -0,0 +1,10 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClientClass implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
