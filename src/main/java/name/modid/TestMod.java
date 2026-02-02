45e44257de990179515fc512ea16140050a2fe02
Andrew Wisniewski <andrew.wisniewski.20@cnu.edu>
2026-02-01 19:41:15 -0500

    Added Pink Garnet item

--- src/main/java/name/modid/TestMod.java
+++ src/main/java/name/modid/TestMod.java
@@ -1,5 +1,6 @@
 package name.modid;
 
+import name.modid.items.ModItemClass;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class TestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemClass.registerModItems();
 	}
 }
---
+++ src/main/java/name/modid/items/ModItemClass.java
@@ -0,0 +1,24 @@
+package name.modid.items;
+
+import name.modid.TestMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItemClass {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> fabricItemGroupEntries.add(PINK_GARNET));
+    }
+}
